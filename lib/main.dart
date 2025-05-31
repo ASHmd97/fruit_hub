75f824cffbc56afcfd86ad78462e5630475a0ad1
ASHmd97 <ahmed.hmd1996@gmail.com>
2025-05-31 16:45:12 +0300

    feat: Integrate Firebase with configuration files and initialize in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruit_hub/core/di/injectable.dart';
@@ -6,11 +7,13 @@ import 'package:fruit_hub/core/routes/app_router.dart';
 import 'package:fruit_hub/core/routes/navigator_observer.dart';
 import 'package:fruit_hub/core/routes/routes.dart';
 import 'package:fruit_hub/core/theme/theme_data/theme_data_light.dart';
+import 'package:fruit_hub/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await configureDependencies();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('en'), Locale('ar')],
